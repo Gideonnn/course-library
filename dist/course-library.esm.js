e8f9adbb23db3ce689209ab20374d8206c258f74
Gideon Spierings <gideongideon@gmail.com>
2022-02-24 16:26:35 +0100

    Remove dist folder from source

--- dist/course-library.esm.js
+++
@@ -1,8 +0,0 @@
-import { createElement } from 'react';
-
-var Thing = function Thing() {
-  return createElement("div", null, "the snozzberries taste like snozzberries");
-};
-
-export { Thing };
-//# sourceMappingURL=course-library.esm.js.map
